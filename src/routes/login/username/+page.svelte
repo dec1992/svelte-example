70afa78f9a7b9bf18d7373aba29b5a9a06e8ca2c
Dec Burns <dec@decs-mbp.home>
2024-10-26 16:57:12 +0100

    confirm usernames

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,5 +1,43 @@
 <script lang="ts">
+	import AuthCheck from '$lib/components/AuthCheck.svelte';
+	import { db } from '$lib/firebase';
+	import { doc, getDoc } from 'firebase/firestore';
 
+	let username = '';
+	let loading = false;
+	let isAvaliable = false;
+	let debounceTimer: NodeJS.Timeout;
+
+	async function checkAvailability() {
+		isAvaliable = false;
+		clearTimeout(debounceTimer);
+		loading = true;
+
+		debounceTimer = setTimeout(async () => {
+			console.log('checking username availability of ', username);
+			const ref = doc(db, 'usernames', username);
+			const exists = await getDoc(ref).then((doc) => doc.exists());
+			isAvaliable = !exists;
+			loading = false;
+		}, 500);
+	}
+
+	async function confirmUsername() {}
 </script>
 
-<h2>Username</h2>
+<AuthCheck>
+	<h2>Username</h2>
+	<form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+		<input
+			type="text"
+			placeholder="Username"
+			bind:value={username}
+			on:input={checkAvailability}
+			class="input w-full"
+		/>
+
+		<p>isAvaliable: {isAvaliable}</p>
+
+		<button class="btn btn-success" disabled={!isAvaliable}>Confirm username @{username}</button>
+	</form>
+</AuthCheck>
